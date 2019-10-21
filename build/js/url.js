7ded553902a93df4f139d141fc1886cd79b994d0
Alec Friedhoff <afriedhoff@gmail.com>
2019-10-21 13:16:12 -0400

    updated paths

---
+++ build/js/url.js
@@ -0,0 +1,12 @@
+var url = {
+    root: "https://c24215cec6c97b637db6-9c0895f07c3474f6636f95b6bf3db172.ssl.cf1.rackcdn.com/interactives/2019/immigration-2018acs/assets/"
+}
+
+//local
+url.root = "./assets/"
+
+url.data = url.root + "json/";
+url.altimg = url.root + "img/";
+url.js = url.root + "js/";
+
+export default url;
