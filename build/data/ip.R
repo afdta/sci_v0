99c8a8241357e6403fdd7c65f206f6c2adb1a6d9
Alec Friedhoff <afriedhoff@gmail.com>
2019-10-21 13:27:13 -0400

    encode cbsa as text

--- build/data/ip.R
+++ build/data/ip.R
@@ -5,7 +5,7 @@ library(jsonlite)
 fyle <- "/home/alec/Projects/Brookings/blogs/startup-complexity/build/data/complexity blog.xlsx"
 
 techiest <- read_excel(fyle, sheet="Table 1", range="B6:F25", col_names=c("sci_rank", "sci", "metro", "share_crunch", "num_advantages"))
-map_data <- read_excel(fyle, sheet="map 1", range="B10:E108", col_names=c("cbsa", "metro", "yf_per1000", "sci"))
+map_data <- read_excel(fyle, sheet="map 1", range="B10:E108", col_names=c("cbsa", "metro", "yf_per1000", "sci"), col_types=c("text", "text", "numeric", "numeric"))
 corrs <- read_excel(fyle, sheet="chart 1", range="A11:D13", col_names=c("correlate", "ba_share", "pci", "sci"))
 
 json <- toJSON(list(table=techiest, map=map_data, chart=corrs), digits=5, pretty=TRUE, na="null")
